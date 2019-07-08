b7756c355819ec35e19117abb0b078ab9bbd2ce1
BrichIvan <britch.ivan@yandex.ru>
2019-07-08 12:09:04 +0300

    Sensors routine works well

--- Sensors.py
+++ Sensors.py
@@ -3,16 +3,18 @@ import ev3dev.ev3 as ev3
 import time
 
 class Sensors(object):
-	def __init__ (self, angle, distance, angle_ac):
+	def __init__ (self, angle, distance):
 		self.angle = angle
-		self.dictance = distance
-		self.angle_ac
+		self.distance = distance
+		#self.angle_ac
 
-	def read_angle(self, angle):
+	def read_angle(self, gy):
 		angle = gy.value()
+		return angle
 
-	def read_distance(self, distance):
+	def read_distance(self, us):
 		distance = us.value()
+		return distance
 '''
 		#Should be called wth high frequency
 	def read_accurate_angle(self, angle_ac)	#Gyro should transmit raw values or speed
@@ -22,34 +24,34 @@ class Sensors(object):
 		millis = int(round(time.time()*1000))
 '''
 
-def initialization():
-	us = UltrasonicSensor()	
-	us.mode = 'US_DIST_CM'	#put the US in the dist in sm mode
+#def initialization(gy, us):
+	#us = ev3.UltrasonicSensor('in4')
+	#us.mode='US-DIST-CM'	#put the US in the dist in sm mode
 
-	gy = GyroSensor()
-	gy.mode = 'GYRO-ANG' #put the gyro into angle mode
+	#gy = ev3.GyroSensor('in3')
+	#gy.mode = 'GYRO-ANG' #put the gyro into angle mode
+
+	#assert gy.connected
+	#assert us.connected
+
+def main():
+	gy = ev3.GyroSensor(ev3.INPUT_3)
+	gy.mode = 'GYRO-ANG'
+	
+	us = ev3.UltrasonicSensor()
+	us.mode = 'US-DIST-CM'
 
 	assert gy.connected
 	assert us.connected
 
-def main():
-	initialization()
-	while (true):
-		Analsys = Sensors(angle, distance, 0)	#Analsys = Sensors(0, 0, 0)
-		Analsys = Sensors.read_angle()
-		#Analsys = Sensors.read_accurate_angle()
-		Analsys = Sensors.read_distance()
-		print (str(Sensors.distance), '/n')
-		time.sleep(0,5)
+	#initialization(gy, us)
+	Analsys = Sensors(0, 0)
+	while (True):
+		angle = Analsys.read_angle(gy)
+		#Analsys  Sensors.read_accurate_angle()
+		distance = Analsys.read_distance(us)
+		print (str(distance), '/n')
+		time.sleep(0.5)
 		#print (str(angle), '/n')
 
 main()
-
-
-
-
-current_millis = int(round(time.time()*1000))
-time.sleep(5)
-delta_millis = current_millis - int(round(time.time()*1000))
-print(delta_millis)
-
